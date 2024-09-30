c29d78e788a2247d574a8bb22a7b555499909f8d
BlueSR10 <167279728+BlueSR10@users.noreply.github.com>
2024-09-30 19:20:16 +0900

    김태현이 잘못 병합함2

---
+++ DFJK/Assets/Scripts/SceneManagers.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagers : MonoBehaviour
+{
+    
+    public void SampleScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void gokselectchang()
+    {
+        SceneManager.LoadScene("gokselectchang");
+    }
+}
